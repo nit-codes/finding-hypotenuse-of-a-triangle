3a3853031354a965d57d590421d402178f4a574f
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-02-03 19:40:53 +0530

    Add files via upload

---
+++ hypotenuse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class hypotenuse {
+    public static void main(String [] args){
+
+        double x;
+        double y;
+        double z;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the side of the triangle: ");
+         x = scanner.nextDouble();
+        System.out.println("Enter the base of the triangle: ");
+         y = scanner.nextDouble();
+
+         z = Math.sqrt((x*x)+(y*y));
+         System.out.println("The hypotenuse of the triangle is "+z);
+    }
+    
+}
